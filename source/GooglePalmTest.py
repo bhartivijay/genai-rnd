f69c0b70cb83242d28fcb4b22806b3258c2d7357
Vijay <vijaybharti321@gmail.com>
2024-06-02 21:27:52 +0530

    Testing

--- source/GooglePalmTest.py
+++ source/GooglePalmTest.py
@@ -18,16 +18,14 @@ hide_github_icon ="""
 header{
   visibility: hidden;
 }
-button{
-visibility: hidden;
-}
+
 </style>
 """
 
 
 st.markdown(hide_github_icon, unsafe_allow_html=True)
 st.header("Ask me a question and I will answer")
-st.markdown('Developed by vijay')
+st.markdown('Developed by Vijay Bharti')
 text_input = st.text_input("Enter Question:")
 value = give_answer(text_input)
 st.write(value)
