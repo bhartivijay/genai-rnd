a7546f6d295d7648437f7e28607be8fb4033b8bc
Vijay <vijaybharti321@gmail.com>
2024-06-13 08:59:55 +0530

    Testing

---
+++ source/ReadFromDoc.py
@@ -0,0 +1,92 @@
+from langchain_google_genai import GoogleGenerativeAI
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+
+key = 'AIzaSyBGHrM4SXVLJlIi7Xb8bNZGX18VxEexvU8'
+
+hide_github_icon ="""
+<style>
+header{
+  visibility: hidden;
+}
+
+</style>
+"""
+
+conversession_chain=None
+#doc=PyPDFLoader.load("asd.pdf")
+
+def readPdfText(pdf_docs):
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+
+def main():
+
+    st.set_page_config("Chat with Multiple PDFs", page_icon=":books:")
+    st.header("Chat with multiple PDFs :books:")
+    #st.markdown(hide_github_icon,unsafe_allow_html=True)
+    user_input = st.text_input("Ask a question from your document")
+
+
+
+    with st.sidebar:
+        st.header("Chat with PDF")
+        st.subheader("Your Document")
+        pdfs=st.file_uploader("Upload pdf files and click process", accept_multiple_files=True,type="pdf")
+        #st.button("Process")
+        if st.button("Process"):
+            with st.spinner("Process"):
+                raw_text=readPdfText(pdfs)
+                chunks=get_text_chunks(raw_text)
+                vectorstore=get_vector_store(chunks)
+                #conversession_chain=getConversastonalChain(vectorstore)
+                st.session_state.conversation = getConversastonalChain(vectorstore)
+                st.success("Done")
+    if st.button("Get Answer"):
+        print("my question is")
+        print(user_input)
+        handleUserInput(user_input)
+
+
+
+
+def get_text_chunks(text):
+    text_splitter=CharacterTextSplitter(separator="\n", chunk_size=1000, chunk_overlap=200)
+    chunks=text_splitter.split_text(text)
+    return chunks
+
+def get_vector_store(text_chunks):
+    embedding=HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    vectorstore=FAISS.from_texts(text_chunks,embedding=embedding)
+    return vectorstore
+
+def getConversastonalChain(vectorstore):
+    llm = GoogleGenerativeAI(model="models/text-bison-001", google_api_key=key, temperature=0.2)
+    #memory=ConversationBufferMemory(memory_key)
+    conversastional_chain=ConversationalRetrievalChain.from_llm(llm=llm,retriever=vectorstore.as_retriever(search_kwargs={"k": 1}),memory=ConversationBufferMemory(memory_key="chat_history",return_message=False,verbose=True,output_key='answer'))
+    return conversastional_chain
+
+
+def handleUserInput(user_question):
+    convers_chain = st.session_state.conversation
+    print('Conversation chain############')
+    print(convers_chain)
+    response=convers_chain.run(user_question)
+    st.write(response)
+
+
+
+
+
+if __name__== "__main__":
+    main()
